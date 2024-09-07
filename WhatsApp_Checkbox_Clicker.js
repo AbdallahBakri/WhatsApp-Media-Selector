bc3762440d04c56499f750fd581c214999cadc4b
A. Bakri <AHBakri@protonmail.com>
2024-09-07 20:16:54 +0400

    Create WhatsApp_Checkbox_Clicker.js

---
+++ WhatsApp_Checkbox_Clicker.js
@@ -0,0 +1,2 @@
+// Paste the code below into the browser's developer console to select all WhatsApp photos and videos:
+document.querySelectorAll('[data-testid="checkbox-round-passive"]').forEach(s => s.click())
